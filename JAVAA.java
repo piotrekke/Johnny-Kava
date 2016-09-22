96ed509c30a0f2261a57b6a6863dd2ca796fae9b
Piotr Duda <t00190919@365s.ittralee.ie>
2016-09-22 10:54:47 +0100

    v

---
+++ JAVAA.java
@@ -0,0 +1,21 @@
+public class message {
+
+    public static void main(String[]args) {
+    
+    int day, month, year;
+    
+    day = 22;
+    
+    month = 9;
+    
+    year = 16;
+    
+    System.out.println("Hi my name is Piotr Duda today is " + day + "/" + month + " + year ");
+    
+    
+    
+    
+    }
+    
+    
+}
